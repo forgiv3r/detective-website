ea7c6d9072b44b33f8f03dddf7f6f795e52bce7e
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-30 00:12:06 +0100

    try to fix contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -77,7 +77,7 @@ export default {
     },
     prepareEmailData() {
       return {
-        to: "jaranwebsite@gmail.com",
+        to: "formularz@jaran.com.pl",
         from: "biuro@jaran.com.pl",
         subject: `Nowa wiadomość z formularza kontaktowego od ${this.form.name}`,
         text: `${this.form.from}: ${this.form.text}`
